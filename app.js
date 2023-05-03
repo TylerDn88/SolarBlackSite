9a6a4c3381bedaf77e1493bf693751eb7caa9251
Niel TD <90249909+TylerDn88@users.noreply.github.com>
2023-05-03 13:23:51 +0200

    update to site with optmizations for SEO
    update to site with optmizations for SEO

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+	mobile_menu.classList.toggle('active');	
 });
 
+window.addEventListener('load',() => {
+		header.style.backgroundColor = '#29323c';	
+});
+/*
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
+	if (scroll_position >= 0) {
 		header.style.backgroundColor = '#29323c';
 	} else {
-		header.style.backgroundColor = 'transparent';
+		header.style.backgroundColor = '#29323c'; //'transparent'
 	}
 });
-
+*/
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
